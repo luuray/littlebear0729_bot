fcf57ac9d121970528bf71c4153d6ef37415bacf
xhn16729 <xiong_haonan@163.com>
2018-08-19 19:34:34 +0800

    add timer feature

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import telebot
 import time
 from datetime import datetime,date
 import datetime
+from telebot import types
+
 random.seed(int(time.time()))
 
 bot = telebot.TeleBot("REPLACE YOUR TOKEN HERE")
@@ -102,5 +104,46 @@ def random_choose(message):
 	else:
 		randNum = random.randint(1, len(element)-1)
 		bot.reply_to(message, element[randNum])
+
+
+#timer
+@bot.message_handler(commands=['timer'])
+def get_time(message):
+	markup = types.ReplyKeyboardMarkup(one_time_keyboard = True)
+
+	itembtn30 = types.KeyboardButton('30秒')
+	itembtn1 = types.KeyboardButton('1分钟')
+	itembtn2 = types.KeyboardButton('2分钟')
+	itembtn5 = types.KeyboardButton('5分钟')
+	itembtncancel = types.KeyboardButton('取消定时')
+
+	markup.row(itembtn30, itembtn1)
+	markup.row(itembtn2, itembtn5)
+	markup.row(itembtncancel)
+	
+	bot.send_message(message.chat.id, "选择要定的时间：", reply_markup = markup)
+	bot.register_next_step_handler(message, timer_set)
+def timer_set(message):
+	markup = types.ReplyKeyboardRemove()
+	
+	if message.text == "30秒" or message.text == "1分钟" or message.text == "2分钟" or message.text == "5分钟":
+		bot.send_message(message.chat.id, "成功定时" + message.text + "！")
+	elif message.text == "取消定时":
+		bot.send_message(message.chat.id, "定时取消！")
+	else:
+		bot.send_message(message.chat.id, "参数错误！")
+
+	if message.text == "30秒":
+		time.sleep(30)
+		bot.send_message(message.chat.id, "30秒到了！")
+	elif message.text == "1分钟":
+		time.sleep(60)
+		bot.send_message(message.chat.id, "1分钟到了！")
+	elif message.text == "2分钟":
+		time.sleep(120)
+		bot.send_message(message.chat.id, "2分钟到了！")
+	elif message.text == "5分钟":
+		time.sleep(300)
+		bot.send_message(message.chat.id, "5分钟到了！")
 
 bot.polling()
